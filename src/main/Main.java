068728ff83cc7c4d863c86e936d898fffcf1d7b2
akazza07 <raiakash1672002@gmail.com>
2025-08-15 22:51:03 +0530

    My first Game

---
+++ src/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This lets the window 
+		                               // properly close when user clicks the close ("X") button.
+		window.setResizable(false);
+		window.setTitle("2D Adventure");
+		
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+		
+		window.pack(); // Causes this window to be sized to fit the 
+		               //preferred size and layouts of its subcomponents (=GamePanel).
+		
+		window.setLocationRelativeTo(null); // Not specify the location of the window 
+		                                // = This window will be displayed at the center of the screen.
+		window.setVisible(true);
+		
+		gamePanel.setupGame();
+        gamePanel.startGameThread();
+       }
+    }
+
