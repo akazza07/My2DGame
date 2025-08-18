ca1ab91b848eba5bafbce6963b1215bf5f9d5e52
akazza07 <raiakash1672002@gmail.com>
2025-08-18 18:46:47 +0530

    ADD NEW TILES

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -21,26 +21,61 @@ public class TileManager {
 		
 		this.gp = gp;
 		
-		tile = new Tile[10];
+		tile = new Tile[50]; // increase the tiles whenever u want
 		mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
 		
 		getTileImage();
 		
-		loadMap("/maps/world01.txt");
+		loadMap("/maps/worldV2.txt");
 	}
 	public void getTileImage() {  // in this we have to add tiles
+		    // we don't use the tile 0 to 9 but I've set a placeholder image so we can prevent NullPointer exception happens when we scan array
 		
+		    // PLACEHOLDER
 			setup(0,"grass",false);
-			setup(1,"wall3",true);
-			setup(2,"water",true);
-			setup(3,"mat1",false);
-			setup(4,"tree",true);
-			setup(5,"sand1",false);
-			setup(6,"wall2",true);
-			setup(7,"banner",false);
-			setup(8,"flower",false);
-			setup(9,"wall1",true);
-		
+			setup(1,"grass",false);
+			setup(2,"grass",false);
+			setup(3,"grass",false);
+			setup(4,"grass",false);
+			setup(5,"grass",false);
+			setup(6,"grass",false);
+			setup(7,"grass",false);
+			setup(8,"grass",false);
+			setup(9,"grass",false);
+			
+			// PLACEHOLDER
+			setup(10,"grass",false);
+			setup(11,"grass2",false);
+			setup(12,"water00",true);
+			setup(13,"water01",true);
+			setup(14,"water02",true);
+			setup(15,"water03",true);
+			setup(16,"water04",true);
+			setup(17,"water05",true);
+			setup(18,"water06",true);
+			setup(19,"water07",true);
+			setup(20,"water08",true);
+			setup(21,"water09",true);
+			setup(22,"water10",true);
+			setup(23,"water11",true);
+			setup(24,"water12",true);
+			setup(25,"water13",true);
+			setup(26,"road00",false);
+			setup(27,"road01",false);
+			setup(28,"road02",false);
+			setup(30,"road03",false);
+			setup(31,"road04",false);
+			setup(32,"road05",false);
+			setup(33,"road06",false);
+			setup(34,"road07",false);
+			setup(35,"road08",false);
+			setup(36,"road09",false);
+			setup(37,"road10",false);
+			setup(38,"road11",false);
+			setup(39,"road12",false);
+			setup(40,"earth",false);
+			setup(41,"wall1",true);
+			setup(42,"tree",true);
 		}
 	public void setup(int index , String imageName , boolean collision) {
 		
