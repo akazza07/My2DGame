54e24eea9c50067bfda0a5da3ffa3cadd1929bc5
akazza07 <raiakash1672002@gmail.com>
2025-08-30 23:38:45 +0530

    Updating Monster

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.NPC_OldMan;
+import monster.MON_GreenSlime;
 import object.OBJ_Door;
 
 public class AssetSetter {
@@ -9,62 +10,23 @@ public class AssetSetter {
 	  this.gp = gp;
   }
   public void setObjects() {
-	 
+
 	  
-	  gp.obj[0] = new OBJ_Door(gp);
-	  gp.obj[0].worldX = 21 * gp.tileSize;
-	  gp.obj[0].worldY = 22  * gp.tileSize;
-	  
-	  gp.obj[1] = new OBJ_Door(gp);
-	  gp.obj[1].worldX = 23 * gp.tileSize;
-	  gp.obj[1].worldY = 25 * gp.tileSize;
-	  
-	  /*
-	  gp.obj[2] = new OBJ_Key(gp);
-	  gp.obj[2].worldX = 38 * gp.tileSize;
-	  gp.obj[2].worldY = 8 * gp.tileSize;
-	  
-	  gp.obj[3] = new OBJ_Door(gp);
-	  gp.obj[3].worldX = 10 * gp.tileSize;
-	  gp.obj[3].worldY = 11  * gp.tileSize;
-	  
-	  gp.obj[4] = new OBJ_Door(gp);
-	  gp.obj[4].worldX = 8 * gp.tileSize;
-	  gp.obj[4].worldY = 28 * gp.tileSize;
-	  
-	  gp.obj[5] = new OBJ_Door(gp);
-	  gp.obj[5].worldX = 12 * gp.tileSize;
-	  gp.obj[5].worldY = 22 * gp.tileSize;
-	  
-	  gp.obj[6] = new OBJ_Chest(gp);
-	  gp.obj[6].worldX = 10 * gp.tileSize;
-	  gp.obj[6].worldY = 7 * gp.tileSize;
-	  
-	  gp.obj[7] = new OBJ_Carrot(gp);
-	  gp.obj[7].worldX = 37 * gp.tileSize;
-	  gp.obj[7].worldY = 42 * gp.tileSize;	
-	                                        */
   }
   public void setNPC() {
 	  gp.npc[0] = new NPC_OldMan(gp);
 	  gp.npc[0].worldX = gp.tileSize*21;
-	  gp.npc[0].worldY = gp.tileSize*21;
+	  gp.npc[0].worldY = gp.tileSize*21;  
+  }
+  public void setMonster() {
 	  
-	  gp.npc[1] = new NPC_OldMan(gp);
-	  gp.npc[1].worldX = gp.tileSize*11;
-	  gp.npc[1].worldY = gp.tileSize*21;
+	  gp.monster[0] = new MON_GreenSlime(gp);
+	  gp.monster[0].worldX = gp.tileSize*23;
+	  gp.monster[0].worldY = gp.tileSize*36;
 	  
-	  gp.npc[2] = new NPC_OldMan(gp);
-	  gp.npc[2].worldX = gp.tileSize*31;
-	  gp.npc[2].worldY = gp.tileSize*21;
-
-	  gp.npc[3] = new NPC_OldMan(gp);
-	  gp.npc[3].worldX = gp.tileSize*21;
-	  gp.npc[3].worldY = gp.tileSize*11;
-	  
-	  gp.npc[4] = new NPC_OldMan(gp);
-	  gp.npc[4].worldX = gp.tileSize*21;
-	  gp.npc[4].worldY = gp.tileSize*31;
+	  gp.monster[1] = new MON_GreenSlime(gp);
+	  gp.monster[1].worldX = gp.tileSize*23;
+	  gp.monster[1].worldY = gp.tileSize*37;
 	  
   }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -48,6 +48,7 @@ public final class GamePanel extends JPanel implements Runnable{
     public Player player = new Player(this,KeyH);
     public Entity obj[] = new Entity[10]; // we prepared 10 slots during the game 
     public Entity npc[] = new Entity[10];
+    public Entity monster[] = new Entity[20];
     ArrayList<Entity>entityList = new ArrayList<>();
     
     // GAME STATE
@@ -76,6 +77,7 @@ public final class GamePanel extends JPanel implements Runnable{
     public void setupGame() {
     	aSetter.setObjects();
     	aSetter.setNPC();
+    	aSetter.setMonster();
     	//playMusic(0);
     	gameState = titleState;
     }
@@ -167,6 +169,11 @@ public final class GamePanel extends JPanel implements Runnable{
 					npc[i].update();
 				}
 			}
+			for(int i = 0; i < monster.length ; i++) {
+				if(monster[i]!= null) {
+					monster[i].update();
+				}
+			}
 		}
 		if(gameState == pauseState) {
 			// nothing
@@ -207,6 +214,11 @@ public final class GamePanel extends JPanel implements Runnable{
             for(int i = 0 ; i < obj.length ; i++) {
             	if(obj[i] != null) {
             		entityList.add(obj[i]);
+            	}
+            }
+            for(int i = 0 ; i < monster.length ; i++) {
+            	if(monster[i] != null) {
+            		entityList.add(monster[i]);
             	}
             }
 		    // PLAYER
